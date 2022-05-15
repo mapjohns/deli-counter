fc878090d3a7258cb0e396a72b27a1fbf986ec75
Matthew <mattj20@gmail.com>
2022-05-15 16:44:25 -0700

    commit tree 447b07ab7ffbae1970763ad45135fab5bf0d3afc

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+require 'pry'
+
+def line(array)
+    counter = 0
+    new_array = []
+    if array.size == 0
+        puts "The line is currently empty."
+    else
+        array.each do |person|
+            new_array << "#{counter+1}. #{person}"
+            counter += 1
+        end
+        test1 = new_array.join(" ")
+        puts "The line is currently: #{test1}"
+    end
+end
+
+def take_a_number(array, name)
+    array << name
+    puts "Welcome, #{name}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+    if array.size == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
